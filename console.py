34e668b707f57591d92d8dc41d828c8a5f5f7669
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-09 12:02:22 +0000

    vscode debugging configuration

--- console.py
+++ console.py
@@ -5,8 +5,8 @@ from models.album import Album
 import repositories.artist_repository as artist_repository
 import repositories.album_repository as album_repository
 
-album_repository.delete_all()
-artist_repository.delete_all()
+#album_repository.delete_all()
+#artist_repository.delete_all()
 
 artist_1 = Artist('Oasis')
 artist_repository.save(artist_1)
